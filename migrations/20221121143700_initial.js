aa057961be1ac97d66ef2db891b1c0ca3d29dc66
Alperen Şafak <alperensafak@Alperens-MacBook-Pro.local>
2022-11-21 17:56:23 +0300

    migrations, created reservations table

---
+++ migrations/20221121143700_initial.js
@@ -0,0 +1,27 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+// exports.up = function(knex) {
+
+// };
+module.exports.up = (knex) =>
+  knex.schema.createTable("reservations", (table) => {
+    table.increments("id").unsigned().primary();
+    table.dateTime("datetime").notNullable();
+    table.specificType("party", "TINYINT").notNullable();
+    table.string("name").notNullable();
+    table.string("email").notNullable();
+    table.string("phone");
+    table.text("message");
+  });
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+// exports.down = function(knex) {
+
+// };
+
+module.exports.down = (knex) => knex.schema.dropTable("reservations");
